7d687e775d53790b887a2eb4ab8e4cc28a57290c
Jacob Norris <62315388+norrisja@users.noreply.github.com>
2021-01-16 22:43:00 -0500

    Update Compare to Average.py

--- Compare to Average.py
+++ Compare to Average.py
@@ -30,18 +30,3 @@ def_vs_avg = defense - def_avg
 off_vs_avg.to_csv('Offense vs Avg.csv')
 def_vs_avg.to_csv('Defense vs Avg.csv')
 pace.to_csv('Pace.csv')
-
-#poss_df = off_prob_df.copy()
-
-#poss_df['FGA%'] = 1 - poss_df['TOV%']
-#poss_df['3P%'] = poss_df['3pt Ratio'] / poss_df['FGA%']
-#poss_df['2P%'] = poss_df['2pt Ratio'] / poss_df['FGA%']
-#poss_df.drop(columns = [])
-
-
-#opp_poss_df = def_prob_df.copy()
-
-#opp_poss_df['FGA%'] = 1 - opp_poss_df['TOV%']
-#opp_poss_df['3P%'] = opp_poss_df['3pt Ratio'] / opp_poss_df['FGA%']
-#opp_poss_df['2P%'] = opp_poss_df['2pt Ratio'] / opp_poss_df['FGA%']
-#poss_df.drop(columns = [])
